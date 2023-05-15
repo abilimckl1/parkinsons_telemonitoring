fcd3df834e182ef3a8f3afbf039df8690345e765
abilimckl1 <mackhemchuah@gmail.com>
2023-05-16 00:37:08 +0800

    'test

--- Parkinsons.R
+++ Parkinsons.R
@@ -44,16 +44,16 @@ range(df_Shimmer$shimmer) #Shimmer without Outliers range
 
 
 #Plotting with outliers
-boxplot.default(df$shimmer, df$shimmer_db, df$shimmer_apq3, df$shimmer_apq5, df$shimmer_apq11, df$shimmer_dda)
+boxplot.default(df[,11:16])
 
 
 #Plotting without outliers
-boxplot.default(df_Shimmer$shimmer, df_Shimmer$shimmer_db, df_Shimmer$shimmer_apq3, df_Shimmer$shimmer_apq5, df_Shimmer$shimmer_apq11, df_Shimmer$shimmer_dda)
+boxplot.default(df_Shimmer[,11:16])
 
 
 #######################################################################
 #Jitter Data Outliers processing
-quartiles_ Jitter <- quantile(df$jitter, probs=c(.25, .75), na.rm = FALSE)
+quartiles_Jitter <- quantile(df$jitter, probs=c(.25, .75), na.rm = TRUE)
 IQR_Jitter <- IQR(df$jitter)
 Lower_Jitter <- quartiles_Jitter[1] - 1.5*IQR_Jitter
 Upper_Jitter <- quartiles_Jitter[2] + 1.5*IQR_Jitter
@@ -73,8 +73,35 @@ range(df_JitterUpper$jitter) #Upper Outlier Jitter Range
 range (df_Jitter$jitter) #Jitter without Outlier Range
 
 #Plotting Jitter with outliers
-boxplot.default(df$jitter, df$jitter_abs, df$jitter_rap, df$jitter_ppq5, df$jitter_ddp)
+boxplot(df[,6:10])
 
 #Plotting Jitter without outliers  ERROR
-boxplot.default(df_Jitter$jitter, df_Jitter$jitter_abs, df_Jitter$jitter_rap, df_Jitter$jitter_ppq5, df_Jitter$jitter_ddp)
+boxplot.default(df_Jitter[,6:10])
+
+
+y <- df[6:21]
+
+#Initializing DF to Null
+parkinson_data_clean <- NULL
+
+for (x in y){
+  remove_outliers <- function(x, na.rm = TRUE, ...) {
+    qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
+    H <- 1.5 * IQR(x, na.rm = na.rm)
+    x[x < (qnt[1] - H)] <- NA
+    x[x > (qnt[2] + H)] <- NA
+    x
+  }
+  
+  parkinson_data_clean <- rbind(parkinson_data_clean,apply(df[, 0:21], 2, remove_outliers))
+  parkinson_data_clean <- na.omit(parkinson_data_clean)
+  
+}
+
+parkinson_data_clean <- as.data.frame(parkinson_data_clean)
+parkinson_data_clean <- distinct(parkinson_data_clean)
+
+boxplot(parkinson_data_clean[6:10])
+boxplot(parkinson_data_clean[11:16])
+boxplot(parkinson_data_clean[16:21])
 
